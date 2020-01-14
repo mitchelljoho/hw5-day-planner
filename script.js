da600b281f807f21d70631ae0380c24d9dbb9b92
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-14 12:37:55 -0800

    almost done

--- script.js
+++ script.js
@@ -1,66 +1,72 @@
 var localStorage = window.localStorage;
-var hourCard = document.getElementById("content-card");
-var timeArr = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-var button = document.querySelector(".save-button");
+var hoursCard = document.getElementById("content-card");
+var clearButton = document.getElementById("reset-button");
 
 // -----------------------------------------------------------------
 // checks if localstorage already exits, and if not, sets a blank json object to work with
-var blankSched = {
-    "9": "a",
-    "10": "b",
-    "11": "c",
-    "12": "d",
-    "1": "e",
-    "2": "f",
-    "3": "g",
-    "4": "h",
-    "5": "i",
-}
+var blankSched = ["", "", "", "", "", "", "", "", ""];
 
-if (localStorage.getItem("schedule") === undefined) {
+var testLocal = localStorage.getItem("schedule");
+if (testLocal === null) {
     localStorage.setItem("schedule", JSON.stringify(blankSched));
 }
 
+var timeArr = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 var schedule = JSON.parse( localStorage.getItem("schedule") );
-var keys = Object.keys(schedule);
-var values = Object.values(schedule);
 // -----------------------------------------------------------------
 
-// draws all time cards from the 
+// draws all time cards from the local storage
 function drawCards() {
-    for(var i = 9; i < (keys.length); i++){
-
+    for(var i = 0; i < (timeArr.length); i++){
         var newCard = document.createElement("div");
         newCard.setAttribute("class", "hour-card");
 
         var saveButton = document.createElement("button");
         saveButton.setAttribute("class", "col-1 save-button");
-        saveButton.setAttribute("value", keys[i]);
+        saveButton.setAttribute("value", timeArr[i]);
         saveButton.textContent = "Save";
 
         var inputCard = document.createElement("input");
         inputCard.setAttribute("type", "text");
+        inputCard.setAttribute("id", "text");
         inputCard.setAttribute("class", "col-10");
         // set background color depending on the time of day
-        inputCard.textContent = values[i];
+        inputCard.value = schedule[i];
 
         var timeCard = document.createElement("div");
         timeCard.setAttribute("class", "col-1");
         timeCard.setAttribute("style", "text-align: right;");
+        if(i > 2){
+            timeCard.textContent = timeArr[i] + "pm";
+        } else {
+            timeCard.textContent = timeArr[i] + "am";
+        }
 
         newCard.appendChild(saveButton);
         newCard.appendChild(inputCard);
         newCard.appendChild(timeCard);
 
-        hourCard.appendChild(newCard);
+        newCard.addEventListener("click", function(e) {
+            if(e.target.localName === "button") {
+                var value = e.target.value;
+                var index = timeArr.indexOf(value);
+                var text = e.path[1].children[1].value;
+                schedule[index] = text;
+                localStorage.setItem("schedule", JSON.stringify(schedule));
+            }
+        });
+
+        hoursCard.appendChild(newCard);
 
     }
 }
 
-document.addEventListener("click", function(e) {
-    if(e.target.textContent = "Save") {
-        var value = e.target.value;
-        var index = keys.indexOf(value);
-        
-    }
-})
+clearButton.addEventListener("click", function(e) {
+    schedule = ["", "", "", "", "", "", "", "", ""];
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+    hoursCard.innerHTML = "";
+    drawCards();
+});
+
+drawCards();
+
